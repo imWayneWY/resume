ae0c4371d83711ea81798acfb5d9d9e6fba92532
weiyan <im.weiyan@foxmail.com>
2019-12-29 10:08:26 -0700

    eject

---
+++ src/components/Resume.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Resume() {
+  return(
+    <div className="root">
+      <div className="profile">
+      </div>
+      <div className="main">
+      </div>
+        <style jsx>{`
+          .root: {
+            backgroune: blue;
+          }
+        `}</style>
+    </div>
+  )
+}
+
+export default Resume
